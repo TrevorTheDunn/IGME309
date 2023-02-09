a084a4318e391ca63eb4397e91bf5622e6cf8d83
Trevor Dunn <thetrevordunn@gmail.com>
2023-02-08 22:31:00 -0500

    Completed Exercise 4

--- E04 - Circle Creation/MyMesh.cpp
+++ E04 - Circle Creation/MyMesh.cpp
@@ -16,9 +16,27 @@ void MyMesh::GenerateCircle(float a_fRadius, int a_nSubdivisions, vector3 a_v3Co
 		Calculate a_nSubdivisions number of points around a center point in a radial manner
 		then call the AddTri function to generate a_nSubdivision number of faces
 	*/
+
+	std::vector<vector3> vertices;
+	GLfloat degree = 0;
+	GLfloat change = static_cast<GLfloat>(PI * 2.0 / static_cast<GLfloat>(a_nSubdivisions));
+
+	for (int i = 0; i < a_nSubdivisions; i++)
+	{
+		vector3 newVec = vector3(cos(degree) * a_fRadius, sin(degree) * a_fRadius, 0.0f);
+		degree += change;
+		vertices.push_back(newVec);
+	}
+
+	for (int i = 0; i < a_nSubdivisions; i++)
+	{
+		AddTri(ZERO_V3, vertices[i], vertices[(i + 1) % a_nSubdivisions]);
+	}
+
+	/*
 	AddTri(	vector3(0.0f, 0.0f, 0.0f),
 			vector3(1.0f, 0.0f, 0.0f),
-			vector3(0.77f, 0.77f, 0.0f));
+			vector3(0.77f, 0.77f, 0.0f));*/
 
 	// Adding information about color
 	CompleteMesh(a_v3Color);
